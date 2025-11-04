f78fb6868bac826f55fdd31e93f7b8273e3156e8
Ilyass <129749723+Ilyas-exe@users.noreply.github.com>
2025-11-04 16:40:21 +0100

    Convert TaskRepository to JPA repository interface
    Refactored TaskRepository from a class to a Spring Data JPA repository interface for Task entities. This enables CRUD operations and future custom queries using Spring Data.

--- backend/todo-service/src/main/java/com/todo/todo_service/repository/TaskRepository.java
+++ backend/todo-service/src/main/java/com/todo/todo_service/repository/TaskRepository.java
@@ -1,5 +1,10 @@
 package com.todo.todo_service.repository;
 
-public class TaskRepository {
-    
+import com.todo.todo_service.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    // We can add custom queries here later
 }
